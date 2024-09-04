860648c7fc5d87bd20b0828502c13d001bcb8405
Tomáš Zlámal <zlamal@tomaszlamal.com>
2024-09-04 16:44:46 +0200

    Added menu animation

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var menu = document.querySelector(".burger-bar");
+var barA = document.querySelector(".bar-a");
+var barB = document.querySelector(".bar-b");
+var barC = document.querySelector(".bar-c");
 var selected = false;
 menu.addEventListener("click", () => {
     var menu_items = document.querySelector(".menu-items");
+    barA.classList.toggle("a");
+    barB.classList.toggle("b");
+    barC.classList.toggle("c"); 
     if(!selected) {
         menu_items.style.width = "100%";
     }
