adfe89821e14f3a700412ee65220b3501be90e84
Tomáš Zlámal <zlamal@tomaszlamal.com>
2024-09-04 17:59:38 +0200

    V1 with links

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ menu.addEventListener("click", () => {
     }
     selected = !selected;
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
